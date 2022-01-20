565e530a9986ea2f24f52bb4637bb51e51b1ce4b
Codiving <codivingmoon@gmail.com>
2022-01-20 20:05:34 +0900

    Main 컴포넌트 추가

---
+++ src/layout/Main/Main.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const Main = styled("section")(() => {
+  return {
+    display: "flex",
+    justifyContent: "center",
+    padding: "2em 4em",
+    gap: "5em"
+  };
+});
+export default Main;
